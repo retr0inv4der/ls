54296b48d3ab5c2b6013b103c67b8a728d6a4146
retr0inv4der <retr0.inv4der@gmail.com>
2026-06-11 21:04:15 +0330

    refactoring even more

--- lsbtw.c
+++ lsbtw.c
@@ -1,38 +1,97 @@
+#include <stdalign.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+#include <errno.h>
 
 
-int show_all = 0 ;
-int long_detail=0;
+struct file_info {
+	char* name ;
+	size_t size ;
+};
 
-int should_show(struct dirent* entry){
-	if(!show_all && entry->d_name[0]=='.' ) return 0 ;
+
+struct flags {
+	bool show_all;
+	bool long_detail;
+};
+struct flags flags ;
+
+void prinf_err(const char* msg){
+	fprintf(stderr,  "%s: %s\n", msg , strerror(errno)) ;
+	fflush(stderr) ;
+}
+
+char*  set_path(int argc , char** argv) {
+	char* res ;
+	res= (argc > optind) ? argv[optind] : "." ;
+	return res ;
+}
+
+
+bool parse_flags(int argc , char** argv){
+	//returns true on success and false on failure
+	bool res = 1 ;
+	char opt ;
+	while((opt = getopt(argc, argv, "al")) != -1){
+		switch (opt) {
+			case 'a' :
+			flags.show_all = true;
+			break;
+
+			case 'l' :
+			flags.long_detail = true;
+			break;
+
+			default:
+			fprintf(stderr, "usage: %s [-a] [-l] [path] "  , argv[0]);
+			fflush(stderr) ;
+			res = 0 ;
+			break;
+			}
+	}
+	return res ;
+}
+
+
+bool should_show(char* name){
+	if(!flags.show_all && name[0]=='.' ) return 0 ;
 	return 1 ;
 }
 
-void print_entry(char* path ,struct dirent* entry){
-	if(long_detail){
-		char fullpath[1024] ;
-		snprintf(fullpath, sizeof(fullpath), "%s/%s", path , entry->d_name);
+void get_file_into(char* path, struct file_info* fi){
+	char full_path[1024] ;
+	snprintf(full_path, sizeof(full_path), "%s/%s", path , fi->name);
 
-		struct stat st ;
-		if(stat(fullpath, &st) != 0  ){
-			perror(fullpath) ;
+	struct stat st ;
+	if(stat(full_path, &st) != 0  ){
+		perror(full_path) ;
 			return;
 		}
+	fi->size = st.st_size ;
+}
 
-		printf("%lu %s\n" , (unsigned long)st.st_size , entry->d_name ) ;
+void print_entry(char * path , char* name ){
+	struct file_info fi ;
+	fi.name = name;
+	if(flags.long_detail){
+		get_file_into(path, &fi) ;
+		printf("%lu %s\n" , (unsigned long)fi.size , fi.name  ) ;
 
 
 	}else{
-		printf("%s    " , entry->d_name) ;
+		printf("%s    " , fi.name) ;
 	}
 
 }
 
-void walk_directory(char* path , void (*callback)(char* , struct dirent* )){
+
+void walk_directory(char* path , void (*callback)( char* , char* )){
 	DIR* directory = opendir(path);
 	if(!directory){
 		perror("opendir") ;
@@ -42,38 +101,24 @@ void walk_directory(char* path , void (*callback)(char* , struct dirent* )){
 
 	struct dirent* entry ;
 	while ((entry = readdir(directory)) != NULL) {
-		if(!should_show(entry)) continue;
-		callback(path , entry) ;
+
+		if(!should_show(entry->d_name)) continue;
+		callback(path , entry->d_name ) ;
 	}
+
+
+	closedir(directory) ;
 }
 
+
 int main(int argc , char* argv[]){
-	//parse argv
-	int opt ;
-	while((opt = getopt(argc, argv, "al")) != -1){
-		switch (opt) {
-			case 'a' :
-				show_all = 1;
-				break;
 
-			case 'l' :
-				long_detail = 1 ;
-				break;
-
-			default:
-				fprintf(stderr, "usage: %s [-a] [path]" , argv[0]);
-				return -1;
-
-		}
+	if(!parse_flags(argc, argv)){
+		exit(1) ;
 	}
-
-	char* path ;
-	if(optind < argc){
-		path = argv[optind] ;
-	}else {
-		path = "." ;
-	}
-	walk_directory(path, print_entry);
+	//create path after parsing the flags acording to the optind var
+	char* path = set_path(argc , argv ) ;
+	walk_directory(path  ,print_entry);
 
 	return 0 ;
 
